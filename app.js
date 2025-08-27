8f03f5f3d7e5e979ce2e41baa89c7673ef3938ae
rhenzu <80802502+rhenzu@users.noreply.github.com>
2025-08-27 14:52:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.use(session({
     secret: process.env.SESSION_SECRET,
